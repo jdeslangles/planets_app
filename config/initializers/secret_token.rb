4c17a86ca480c419d3ab27a424542dc2a07c9c89
jdeslangles <julien.deslangles@gmail.com>
2013-07-03 13:27:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanetsApp::Application.config.secret_token = 'fe567e86ed549a5b565ff07b02b3f4611fc9892eb46fad5a435878f5299e62890a8d4bb4b1a1b52ba392e73408aa9e0a0cec209946338cffb4abc5c300954068'
